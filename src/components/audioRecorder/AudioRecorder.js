af267d0f33c961656adb31a82149807c172b0c83
luismlhdz <luismlhdz@gmail.com>
2021-06-07 20:00:38 -0500

    comment remove

--- src/components/audioRecorder/AudioRecorder.js
+++ src/components/audioRecorder/AudioRecorder.js
@@ -6,20 +6,17 @@ const RecordView = () => {
     
   return (
     <div className="record-view">
-      <audio src={audioURL} controls />
-      <button onClick={startRecording} disabled={isRecording}>
-        start recording
-      </button>
-      <button onClick={stopRecording} disabled={!isRecording}>
-        stop recording
-      </button>
-
-      <p>
-        <em>
-          (On Codesandbox pop out the preview into a window to get a user media
-          request.)
-        </em>
-      </p>
+      <div className="">
+        <audio src={audioURL} controls />
+      </div>
+      <div className="">
+        <button onClick={startRecording} disabled={isRecording}>
+          start recording
+        </button>
+        <button onClick={stopRecording} disabled={!isRecording}>
+          stop recording
+        </button>
+      </div>
     </div>
   );
 }
